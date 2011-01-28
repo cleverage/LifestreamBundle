43f916fa10219d73b3257488b5ae875bd570b249
Romain Pouclet <me@palleas.com>
2011-01-28 12:36:56 +0100

    Added index action.

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ApplicationLifestreamBundle\Controller;
+namespace Application\LifestreamBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ApplicationLifestreamBundle:Default:index.html.twig');
+        return $this->render('LifestreamBundle:Default:index.html.twig');
     }
 }
