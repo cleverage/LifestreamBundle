3e4365d8e51c6a636cbf39e8574b786a68a78b01
Romain Pouclet <me@palleas.com>
2011-02-01 09:45:04 +0100

    First unit tests

---
+++ Tests/LastFmTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Application\LifestreamBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+use Application\LifestreamBundle\Service\LastFmAPI;
+
+require __DIR__.'/../../../autoload.php';
+
+/**
+* 
+*/
+class LastFmTest extends TestCase
+{
+    const SAMPLE_KEY = 'ohaidizizmykey';
+    
+    const SAMPLE_SECRET = 'ohaidizizmysecret';
+    
+    public function testProfileUrlWithSampleUsername() {
+        $handler = new LastFmAPI(self::SAMPLE_KEY, self::SAMPLE_SECRET, 'palleas', null);
+        $this->assertEquals('http://www.lastfm.fr/user/palleas', $handler->getProfileUrl());
+    }
+}
