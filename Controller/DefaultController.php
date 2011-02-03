d9f52ea6dfbaae7bdc775dccf106cb0bbe52918e
Romain Pouclet <me@palleas.com>
2011-02-03 21:19:29 +0100

    Enabled caching for index controller

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,25 +3,43 @@
 namespace Application\LifestreamBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller {
 
     public function indexAction() {
+        $request = $this->get('request');
 
-        return $this->render('LifestreamBundle:Default:index.html.twig', array(
-            'services' => array('lastfm', 'flickr')
-        ));
-    }
-    
-    public function showAction($service) {
+        $response = new Response();
         
+        if (!$response->isNotModified($request))
+        {
+            $response->setMaxAge(600);
+            $response->setPublic();
+            
+            return $this->render('LifestreamBundle:Default:index.html.twig', array(
+                'services' => array('lastfm', 'flickr')
+                ), $response);
+        }
+        else
+        {
+            $response->setNotModified();
+            $response->send();
+        }
+    }
+
+    public function showAction($service) {
+
         $handler = $this->get(sprintf('lifestream.%s.api', $service));
         $recents = $handler->getRecents();
-        
-        return $this->render(sprintf('LifestreamBundle:Default:show_%s.html.twig', $service), 
+
+        $response = $this->render(sprintf('LifestreamBundle:Default:show_%s.html.twig', $service), 
         array(
             'service' => $service, 
             'recents' => $recents
             ));
+
+
+        return $response;
     }
 }
